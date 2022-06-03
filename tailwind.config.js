e884b1983de6eb6b478622bfe801db1465d393a4
Shin-sibainu <shincode0712@gmail.com>
2022-06-03 19:17:51 +0900

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
     extend: {
       colors: {
         body: "#050505",
-        "selected-text": "#A3A3FF",
+        selectedText: "#A3A3FF",
         theme: "#5c318c",
         nav: "#404053",
         secondary: "#9191A4",
         badge: "#3F3F51",
-        "input-border": "#565666",
-        input: "#2A2A35",
+        inputBorder: "#565666",
+        input: "#2A2A33",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
